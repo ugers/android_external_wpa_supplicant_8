b033df10eb1d18c353c9a36a6a5293e39dd2ddad
ugers <yugers@gmail.com>
2014-04-13 18:35:42 +0400

    Update version.h

--- src/common/version.h
+++ src/common/version.h
@@ -7,4 +7,12 @@
 
 #define VERSION_STR "2.0-devel" VERSION_STR_POSTFIX
 
+#ifdef REALTEK_WIFI_VENDOR
+	#include "rtw_version.h"
+	#ifdef RTW_VERSION
+		#undef VERSION_STR
+		#define VERSION_STR "2.0-devel" VERSION_STR_POSTFIX "_" RTW_VERSION
+	#endif
+#endif
+
 #endif /* VERSION_H */
